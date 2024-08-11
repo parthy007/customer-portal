cda7d48127b2e04bcb6585f4fbee8b289c1b57c0
parthy007 <parthadhikari1812@gmail.com>
2024-08-11 10:58:17 +0530

    Revert "Remove extra state"
    This reverts commit 7e3bcd0e21d63fccee243e7515d61a0c42492591.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import "./App.css"
 import { useState } from 'react';
 import CustomerList from './Components/CustomerLists/CustomerLists';
 import CustomerDetails from './Components/CustomerDetails/CustomerDetails';
+import { Customer } from './interfaces';
 import { customersConstants } from './Constants';
 
 const App: React.FC = () => {
+  const [customers, setCustomers] = useState<Customer[]>(customersConstants);
   const [selectedCustomerId, setSelectedCustomerId] = useState<number>(1);
 
   const handleSelectCustomer = (id: number) => {
     setSelectedCustomerId(id);
   };
 
-  const selectedCustomer = customersConstants.find(c => c.id === selectedCustomerId);
+  const selectedCustomer = customers.find(c => c.id === selectedCustomerId);
 
   return (
     <div className="app">
